ee7c028538a4b54ab460d036f39064690d9d9ff8
One2r <601941036@qq.com>
2016-09-21 16:55:01 +0800

    添加main文件

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Download_Files()
+{
+    local URL=$1
+    local FileName=$2
+    if [ -s "${FileName}" ]; then
+        echo "${FileName} [found]"
+    else
+        echo "Notice: ${FileName} not found!!!download now..."
+        wget -c --progress=bar:force ${URL}
+    fi
+}
